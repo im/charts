b46d833c7136d4a7a57207bc94f51070340d31c7
tangxiaomi <81195314@qq.com>
2023-04-21 14:12:05 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,10 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 // import routes from '~pages'
 
-import Home from '@/components/pages/home'
+// import Home from '@/components/pages/home'
 
 const routes = [
-    { path: '/', component: Home }
+    { path: '/', component: import('@/components/pages/home') }
 ]
 const router = createRouter({
     history: createWebHashHistory(),
